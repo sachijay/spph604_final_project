f13d740f3865c39a1adde65ddaad35f34053446d
Pramoda Sachinthana Jayasinghe <30071227+sachijay@users.noreply.github.com>
2023-09-25 16:26:49 -0700

    Added code for table 1

--- r_code/2_sample_explore.r
+++ r_code/2_sample_explore.r
@@ -62,3 +62,42 @@ dat %>%
     copd_or_others,
     has_insurance
   )
+
+
+## Generate table 1 ####
+
+dat_table_1 <- dat %>% 
+  labelled::set_variable_labels(
+    copd_or_others = "Have/Had COPD, emphysema, ChB",
+    has_insurance = "Has insurance",
+    age_years = "Age (years)",
+    relative_asthma = "Close relative with asthma",
+    asthma_ed_visits_year = "ED visits for asthma/past yr",
+    lung_cancer = "Lung cancer",
+    smoked_100_life = "Smoked more than 100 cigarettes/life",
+    num_smoke_inside = "# of people who smoke inside"
+  )
+
+table1::table1(
+  ~ has_insurance + age_years + relative_asthma + asthma_ed_visits_year + lung_cancer + smoked_100_life + num_smoke_inside | copd_or_others,
+  data = dat_table_1,
+  overall = c(left = "Total"),
+  footnote = "ChB = Chronic bronchitis"
+)
+
+table_1 <- tableone::CreateTableOne(
+  vars = c("has_insurance", "age_years", "relative_asthma", "asthma_ed_visits_year", "lung_cancer", "smoked_100_life", "num_smoke_inside"),
+  strata = "copd_or_others",
+  data = dat_table_1,
+  addOverall = TRUE,
+  includeNA = TRUE, 
+  test = FALSE
+)
+
+print(
+  table_1, 
+  showAllLevels = TRUE, 
+  varLabels = TRUE,
+  
+)
+
