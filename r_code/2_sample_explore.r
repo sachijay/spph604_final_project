3807e2abb8e2197ea359a4c272b3529e0ce59cb9
Pramoda Sachinthana Jayasinghe <30071227+sachijay@users.noreply.github.com>
2023-10-14 16:08:30 -0700

    Completed gt_summary table 1

--- r_code/2_sample_explore.r
+++ r_code/2_sample_explore.r
@@ -74,64 +74,34 @@ dat_analytic %>%
 
 ## Generate table 1 ####
 
-dat_table_1 <- survey_design %>% 
-  labelled::set_variable_labels(
-    copd_or_others = "Have/Had COPD",
-    has_insurance = "Has insurance",
-    age_years = "Age (years)",
-    relative_asthma = "Close relative with asthma",
-    asthma_ed_visits_year = "ED visits for asthma/past yr",
-    lung_cancer = "Lung cancer",
-    smoked_100_life = "Smoked more than 100 cigarettes/life",
-    num_smoke_inside = "# of people who smoke inside"
+survey_design_table1 <- survey_design
+
+survey_design_table1$variables <- survey_design_table1$variables %>% 
+  mutate(
+    num_smoke_inside = num_smoke_inside %>% 
+      as.factor(),
+    across(
+      .cols = c(relative_asthma, asthma_ed_visits_year, lung_cancer, smoked_100_life, num_smoke_inside),
+      .fns = ~ fct_na_value_to_level(.x, level = "Missing")
+    )
   )
 
-table1::table1(
-  ~ has_insurance + age_years + relative_asthma + asthma_ed_visits_year + lung_cancer + smoked_100_life + num_smoke_inside | copd_or_others,
-  data = dat_table_1,
-  overall = c(left = "Total"),
-  footnote = "ChB = Chronic bronchitis"
-)
-
-table_1 <- tableone::CreateTableOne(
-  vars = c("has_insurance", "age_years", "relative_asthma", "asthma_ed_visits_year", "lung_cancer", "smoked_100_life", "num_smoke_inside"),
-  strata = "copd_or_others",
-  data = dat_table_1,
-  addOverall = TRUE,
-  includeNA = TRUE, 
-  test = FALSE
-)
-
-print(
-  table_1, 
-  showAllLevels = TRUE, 
-  varLabels = TRUE,
-  
-)
-
-tableone::svyCreateTableOne(
-  vars = c("has_insurance", "age_years", "relative_asthma", "asthma_ed_visits_year", "lung_cancer", "smoked_100_life", "num_smoke_inside"),
-  strata = "copd_or_others",
-  data = survey_design,
-  addOverall = TRUE,
-  includeNA = TRUE, 
-  test = FALSE
-) %>% 
-  print(
-    format = "p"
-  )
-
-library(gtsummary)
 
 tab1 <- tbl_svysummary(
-  data = survey_design,
+  data = survey_design_table1,
   by = copd_or_others,
   include = c(has_insurance, age_years, relative_asthma, asthma_ed_visits_year, lung_cancer, smoked_100_life, num_smoke_inside),
-  percent = "cell",
+  percent = "column",
   statistic = list(
-    # all_continuous() ~ "{median} ({p25}, {p75})", 
-    all_continuous() ~ "{mean} ({sd})", 
+    all_continuous() ~ "{median} ({p25}, {p75})",
     all_categorical() ~ "{p}%"
+  ),
+  type = list(
+    where(is.factor) ~ "categorical" ## To show both levels of dichotomous variables
+  ),
+  digits = list(
+    all_continuous() ~ 1,
+    all_categorical() ~ 0
   ),
   label = list(
     has_insurance ~ "Has insurance",
@@ -149,7 +119,6 @@ tab1 <- tbl_svysummary(
   modify_header(
     label = "**Characteristic**",
     stat_0 = "**Overall**",
-    stat_1 = "**Has COPD",
-    stat_2 = "**Doesn't have COPD"
+    stat_1 = "**Has COPD**",
+    stat_2 = "**Doesn't have COPD**"
   )
-
