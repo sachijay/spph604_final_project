a9b3b2729d9e670a4d78f14de28770f8ade5e875
Pramoda Sachinthana Jayasinghe <30071227+sachijay@users.noreply.github.com>
2023-10-16 22:27:38 -0700

    Generated ORs ignoring sampling design

--- r_code/3_run_model.R
+++ r_code/3_run_model.R
@@ -15,6 +15,22 @@ load(
 
 ## Run model ####
 
+### Unweighted ####
+
+model_base_unweighted <- glm(
+  copd_or_others ~ has_insurance + n_times_healthcare_visit + smoking_status + num_smoke_inside + have_diabetes + age_years + sex,
+  data = dat_analytic,
+  family = binomial(link = "logit")
+)
+
+result_mod_base_unweighted <- tbl_regression(
+  model_base_unweighted,
+  exponentiate = TRUE
+)
+
+
+### Weighted ####
+
 model_base <- svyglm(
   copd_or_others ~ has_insurance + n_times_healthcare_visit + smoking_status + num_smoke_inside + have_diabetes + age_years + sex,
   design = survey_design,
