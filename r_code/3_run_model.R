b75388cd671d8413640ffae5df4f8b161d63c516
Pramoda Sachinthana Jayasinghe <30071227+sachijay@users.noreply.github.com>
2023-10-16 21:35:29 -0700

    Added diabetes and sex into the glm

--- r_code/3_run_model.R
+++ r_code/3_run_model.R
@@ -16,7 +16,7 @@ load(
 ## Run model ####
 
 model_base <- svyglm(
-  copd_or_others ~ has_insurance + age_years + relative_asthma + asthma_ed_visits_year + lung_cancer + smoked_100_life + num_smoke_inside,
+  copd_or_others ~ has_insurance + n_times_healthcare_visit + smoking_status + num_smoke_inside + have_diabetes + age_years + sex,
   design = survey_design,
   family = binomial(link = "logit")
 )
