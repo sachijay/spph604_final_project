4ad3a8454b872099c1570c93d6943eb2bfcc7ad8
Pramoda Sachinthana Jayasinghe <30071227+sachijay@users.noreply.github.com>
2023-11-09 18:59:43 -0800

    Added ROC and AUC codes

--- r_code/3_run_model.r
+++ r_code/3_run_model.r
@@ -29,16 +29,6 @@ result_mod_base_unweighted <- tbl_regression(
 )
 
 
-
-## Model diagnostics
-
-model_base_unweighted_auc <- WeightedROC::WeightedAUC(
-  WeightedROC::WeightedROC(
-    
-  )
-)
-
-
 ### Weighted ####
 
 model_base_crude <- svyglm(
@@ -57,3 +47,65 @@ result_mod_base <- tbl_regression(
   model_base,
   exponentiate = TRUE
 )
+
+
+## Model diagnostics ####
+
+### ROC ####
+
+gof_dat_unweighted_roc <- WeightedROC::WeightedROC(
+  guess = predict(model_base_unweighted, type = "response"),
+  label = dat_analytic_no_miss$copd_or_others
+) %>% 
+  as_tibble() %>% 
+  add_column(
+    model = "Unweighted",
+    .before = 1
+  )
+
+gof_dat_weighted_roc <- WeightedROC::WeightedROC(
+  guess = predict(model_base, type = "response"),
+  label = dat_analytic_no_miss$copd_or_others,
+  weight = dat_analytic_no_miss$interview_wt_adj
+) %>% 
+  as_tibble() %>% 
+  add_column(
+    model = "Weighted",
+    .before = 1
+  )
+
+gof_dat_all_mod_roc <- bind_rows(
+  gof_dat_unweighted_roc,
+  gof_dat_weighted_roc
+)
+
+
+## Plot the ROC curves
+
+gof_plot_all_mod <- gof_dat_all_mod_roc %>% 
+  ggplot(
+    aes(
+      x = FPR,
+      y = TPR,
+      colour = model
+    )
+  ) + 
+  geom_path() + 
+  coord_equal() +
+  labs(
+    colour = "Model"
+  ) +
+  theme_classic()
+
+
+### Area under the curve (AUC) ####
+
+model_base_unweighted_auc <- WeightedROC::WeightedAUC(
+  gof_dat_unweighted_roc
+)
+
+model_base_weighted_auc <- WeightedROC::WeightedAUC(
+  gof_dat_weighted_roc
+)
+
+
